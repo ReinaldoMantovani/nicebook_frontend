851672f3ca046f2bb70bd61ca01cf1550fc63fda
ReinaldoMantovani <reinaldotha88@gmail.com>
2024-09-13 00:52:07 -0300

    Create single page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,11 +3,13 @@ import Header from '../../components/Header/Header'
 import { Outlet } from 'react-router-dom';
 import BookList from '../../components/BookList/BookList';
 
+
  const Home = () => {
   return (
     <main>
        <Header/> 
        <BookList />
+      
        <Outlet/>
     </main>
   )
